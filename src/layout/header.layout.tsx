d05ba97c22bcb4eb6a73138f5128aafd9c0316ec
prescottbreeden <prescott.breeden@gmail.com>
2021-01-15 20:58:35 -0800

    added icons and fixed mobile nav

--- src/layout/header.layout.tsx
+++ src/layout/header.layout.tsx
@@ -1,6 +1,5 @@
 import React, {useEffect, useState} from 'react';
 import { Waypoint } from 'react-waypoint';
-import { Link } from 'react-router-dom';
 import {
   compose,
   either,
@@ -8,9 +7,9 @@ import {
   headerHeight,
   maybe,
   prop,
-  scrollToAnchor,
   subtract,
 } from '../utils';
+import {Navigation} from './navigation.layout';
 
 
 interface HeaderProps {}
@@ -18,18 +17,14 @@ export const Header: React.FC<HeaderProps> = () => {
   // -- local state --
   const [navClass, setNavClass] = useState("nav");
 
-  // -- component logic --
-  // setSticky :: ele -> undefined
-  const setSticky = compose(
-    either(() => setNavClass("nav sticky"), () => setNavClass("nav")),
-    gt(0),
-    subtract(window.pageYOffset),
-    prop('offsetTop')
-  );
-
   // -- lifecycle --
   useEffect(() => {
-    maybe(document.getElementById('nav')).map(setSticky);
+    maybe(document.getElementById('nav')).map(compose(
+      either(() => setNavClass("nav sticky"), () => setNavClass("nav")),
+      gt(0),
+      subtract(window.pageYOffset),
+      prop('offsetTop')
+    ));
   });
 
   // -- display logic --
@@ -47,68 +42,9 @@ export const Header: React.FC<HeaderProps> = () => {
             </h1>
             <p className="header__secondary-text">Software Engineer</p>
           </div>
-          <div className="cb-nav">
-            <div className="cb-nav__container">
-              <input type="checkbox" className="cb-nav__checkbox" id="navi-toggle" />
-              <label htmlFor="navi-toggle" className="cb-nav__button">
-                <span className="cb-nav__icon">&nbsp;</span>
-              </label>
-            </div>
-          </div>
         </header>
       </Waypoint>
-      <nav className={navClass} id="nav">
-        <div className="nav__list">
-          <Link to="/">
-            <div 
-              className="nav__item nav__item--home about-nav"
-              onClick={() => scrollToAnchor('fold')}
-            >
-              <svg className="nav__icon">
-                <use 
-                  xlinkHref="./../assets/img/icomoon/sprite.svg#icon-profile">
-                </use>
-              </svg>
-                <span className="nav__link">About</span>
-            </div>
-          </Link>
-          <Link to="/portfolio">
-            <div
-              className="nav__item portfolio-nav"
-              onClick={() => scrollToAnchor('fold')}
-            >
-              <svg className="nav__icon nav__icon--portfolio">
-                <use 
-                  xlinkHref="./../assets/img/icomoon/sprite.svg#icon-embed2">
-                </use>
-              </svg>
-              <span className="nav__link">
-                  Portfolio
-              </span>
-            </div>
-          </Link>
-          <div 
-            className="nav__item tech-nav"
-            onClick={() => scrollToAnchor('fold')}
-          >
-            <svg className="nav__icon navigation__icon--about">
-              <use 
-                href="./../assets/img/icomoon/sprite.svg#icon-database">
-              </use>
-            </svg>
-            <span className="nav__link">Technology</span>
-          </div>
-          <div 
-            className="nav__item contact-nav"
-            onClick={() => scrollToAnchor('fold')}
-          >
-            <svg className="nav__icon navigation__icon--contact">
-              <use href="/assets/img/icomoon/sprite.svg#icon-envelop"/>
-            </svg>
-            <span className="nav__link">Contact</span>
-          </div>
-        </div>
-      </nav>
+      <Navigation navClass={navClass} />
     </>
   );
 };
---
+++ src/layout/navigation.layout.tsx
@@ -0,0 +1,112 @@
+import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
+import {Icon} from '../components/Icon.component';
+import {compose, equals, maybe, prop, scrollToAnchor} from '../utils';
+
+interface NavigationProps {
+  navClass: string;
+}
+export const Navigation: React.FC<NavigationProps> = ({ navClass }) => {
+  // -- local state --
+  const [checkboxNav, setCheckboxNav] = useState(false);
+
+  // -- component logic --
+  const handleToggle = () => {
+    setCheckboxNav(!checkboxNav);
+  };
+  const handleNav = () => {
+    scrollToAnchor('fold');
+    setCheckboxNav(false);
+  };
+  const getComputedStyle = (element: any) => {
+    return window.getComputedStyle(element);
+  }
+  // -- display logic --
+  const computedClassName = (bool: boolean) => {
+    return bool ? navClass : checkboxNav ? 'nav active-nav' : 'nav';
+  };
+  const navClassName = () => {
+    return maybe(document.getElementById('nav')).map(compose(
+      computedClassName,
+      equals('absolute'),
+      prop('position'),
+      getComputedStyle
+    )).join();
+  };
+
+  return (
+    <>
+      <div className="cb-nav">
+        <div className="cb-nav__container">
+          <input
+            type="checkbox"
+            className="cb-nav__checkbox"
+            id="navi-toggle" 
+            checked={checkboxNav}
+          />
+          <label
+            htmlFor="navi-toggle"
+            className="cb-nav__button"
+            onClick={handleToggle}
+          >
+            <span className="cb-nav__icon">&nbsp;</span>
+          </label>
+        </div>
+      </div>
+      <nav
+        className={navClassName()}
+        id="nav"
+      >
+        <div className="nav__list">
+          <Link to="/">
+            <div 
+              className="nav__item nav__item--home about-nav"
+              onClick={handleNav}
+            >
+              <span className="nav__link">About</span>
+              <Icon 
+                className="nav__icon"
+                name="profile"
+              />
+            </div>
+          </Link>
+          <Link to="/portfolio">
+            <div
+              className="nav__item portfolio-nav"
+              onClick={handleNav}
+            >
+              <span className="nav__link">
+                  Portfolio
+              </span>
+              <Icon 
+                className="nav__icon nav__icon--portfolio"
+                name="embed"
+              />
+            </div>
+          </Link>
+          <div 
+            className="nav__item tech-nav"
+            onClick={handleNav}
+          >
+            <span className="nav__link">Technology</span>
+            <Icon 
+              className="nav__icon nav__icon--about"
+              name="database"
+            />
+          </div>
+          <div 
+            className="nav__item contact-nav"
+            onClick={handleNav}
+          >
+            <span className="nav__link">Contact</span>
+            <Icon 
+              className="nav__icon nav__icon--contact"
+              name="envelop"
+            />
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
