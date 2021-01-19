500649465ffe48de8f76603c769fe8af8beeef68
prescottbreeden <prescott.breeden@gmail.com>
2021-01-18 17:20:45 -0800

    dynamic styling for new skill cards

--- src/components/Skills.component.tsx
+++ src/components/Skills.component.tsx
@@ -7,15 +7,15 @@ export const Tools: React.FC<ToolsProps> = (props) => {
       <section className="tools">
         <div className="tools__row">
           <div className="tools__list">
-            <h2>Skills</h2>
-            <p>Software Design</p>
-            <p>Functional Programming</p>
-            <p>Object Oriented Programming</p>
-            <p>Authentication</p>
+            <h2 className="tools__list__title">Skills</h2>
+            <p>Dynamic Website Content</p>
+            <p>State Management</p>
+            <p>Authentication & Validation</p>
+            <p>Data Access Layers / Microservices</p>
             <p>Public Speaking</p>
           </div>
           <div className="tools__list">
-            <h2>Languages</h2>
+            <h2 className="tools__list__title">Languages</h2>
             <p>JavaScript / TypeScript</p>
             <p>C#</p>
             <p>F#</p>
@@ -23,7 +23,7 @@ export const Tools: React.FC<ToolsProps> = (props) => {
             <p>PHP</p>
           </div>
           <div className="tools__list">
-            <h2>Front End</h2>
+            <h2 className="tools__list__title">Front End</h2>
             <p>React</p>
             <p>Redux</p>
             <p>SASS</p>
@@ -31,7 +31,7 @@ export const Tools: React.FC<ToolsProps> = (props) => {
             <p>Elm</p>
           </div>
           <div className="tools__list">
-            <h2>Back End</h2>
+            <h2 className="tools__list__title">Back End</h2>
             <p>Node / Express</p>
             <p>ASP.NET Core</p>
             <p>Electron</p>
@@ -39,7 +39,7 @@ export const Tools: React.FC<ToolsProps> = (props) => {
             <p>Next.js</p>
           </div>
           <div className="tools__list">
-            <h2>Data</h2>
+            <h2 className="tools__list__title">Data</h2>
             <p>MongoDB</p>
             <p>MySQL</p>
             <p>Microsoft SQL Server</p>
@@ -47,7 +47,7 @@ export const Tools: React.FC<ToolsProps> = (props) => {
             <p>GraphQL</p>
           </div>
           <div className="tools__list">
-            <h2>ORMs / ODMs</h2>
+            <h2 className="tools__list__title">ORMs / ODMs</h2>
             <p>Mongoose</p>
             <p>Entity Framework</p>
             <p>Dapper</p>
